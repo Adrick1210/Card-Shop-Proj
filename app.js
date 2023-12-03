4569537d6042cb7732b6440b44014a880717016f
Christian Alexander <christian.alexanderato@gmail.com>
2023-12-03 00:10:41 -0800

    can now fetch specific key value pairs from my data

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 // fetch data
-// define a function that grabs my card name for now
 
+// define a function that grabs my card by name
 function getCard(name) {
   // fetch from the data url
-  const cardName = fetch(
-    `https://db.ygoprodeck.com/api/v7/cardinfo.php?name=${name}`
-  );
-  cardName
-    .then((res) => {
-      return res.json();
-    })
-    .then((card) => {
-      console.log(card);
+  fetch(`https://db.ygoprodeck.com/api/v7/cardinfo.php?name=${name}`)
+  // return the response json
+    .then((res) => res.json())
+    // access the data
+    .then((data) => {
+      console.log(data);
+      // target specific key value pairs
+      const cardName = data.data[0].name;
+      const cardPrice = data.data[0].card_prices[0].tcgplayer_price;
+
+      // test log to view information
+      console.log(`${cardName}, price: ${cardPrice}`)
+      // print the information to the page
+      printName(`${cardName}`, `${cardPrice}`);
     });
 }
 
 // functions
-
-
+function printName(card, price) {
+  const cardDiv = document.querySelector(".card");
+  cardDiv.innerHTML = `
+  <h1>${card}:</h1>
+  <h2>Price: $${price}</h2>
+  <h2></h2>`;
+}
 // main code
-getCard("red-eyes black dragon");
+// call for the targeted piece of data examples:
+// getCard("Dark Magician Girl");
+getCard('Borreload Savage Dragon');
