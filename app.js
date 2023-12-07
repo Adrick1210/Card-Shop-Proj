0811c8e88d0ecaa2be66ecf1e5f6e3ac0b530484
Christian Alexander <christian.alexanderato@gmail.com>
2023-12-06 21:58:59 -0800

    Functionality working as designed

--- app.js
+++ app.js
@@ -19,8 +19,9 @@ function getCard(name) {
       priceInfo = [];
       setInfo = [];
 
-      // name of card for now
       const cardName = data.data[0].name;
+      // card image sized for mobile view
+      const cardImgSma = data.data[0].card_images[0].image_url;
       // target card prices and print out each price of the card via its market
       const cardPrice = data.data[0].card_prices[0];
       for (let prices in cardPrice) {
@@ -32,8 +33,7 @@ function getCard(name) {
         priceTemplate.price = cardPrice[prices];
         priceInfo.push(priceTemplate);
       }
-      // card image small sized for mobile view
-      const cardImgSma = data.data[0].card_images[0].image_url_small;
+
       // target the card sets I want to print
       const cardSets = data.data[0].card_sets;
       for (let sets of cardSets) {
@@ -45,22 +45,22 @@ function getCard(name) {
         setTemplate.rarity = sets.set_rarity;
         setInfo.push(setTemplate);
       }
-      console.log(data);
-
+      const fiveRandomSets = fiveRanSets(setInfo);
+      
       // print the information to the page
       printPriceHead();
       // I loop over the array of prices and invoke my function to print the information
       for (let i = 0; i < 5; i++) {
-        printPrice(priceInfo[i].market.replace("_", " ").toUpperCase() + ":");
+        printPHead(`${priceInfo[i].market.replace('_', '-')}:`);
         printPrice(`$${priceInfo[i].price}`);
       }
 
       printSetHead();
-      // I loop over my sets to a certain range
-      for (let i = 0; i < 6; i++) {
-        printSet(`- ${setInfo[i].set} -`);
-        printSet(setInfo[i].rarity);
+      for (let i = 0; i < 5; i++) {
+        printSHead(fiveRandomSets[i].set);
+        printSet(fiveRandomSets[i].rarity);
       }
+
       // I print the card name and image
       printName(`${cardName}`, `${cardImgSma}`);
     });
@@ -80,6 +80,14 @@ function printName(card, image) {
 function printPrice(market) {
   const priceTest = document.querySelector("#price");
   const newDiv = document.createElement("div");
+  newDiv.classList.add("price");
+  newDiv.innerText = market;
+  priceTest.appendChild(newDiv);
+}
+
+function printPHead(market) {
+  const priceTest = document.querySelector("#price");
+  const newDiv = document.createElement("div");
   newDiv.classList.add("seller");
   newDiv.innerText = market;
   priceTest.appendChild(newDiv);
@@ -88,6 +96,15 @@ function printPrice(market) {
 function printSet(cardSet) {
   const setTest = document.querySelector("#set");
   const newDiv = document.createElement("div");
+  newDiv.classList.add("rating");
+  newDiv.innerText = cardSet;
+  setTest.appendChild(newDiv);
+}
+
+function printSHead(cardSet) {
+  const setTest = document.querySelector("#set");
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("set-name");
   newDiv.innerText = cardSet;
   setTest.appendChild(newDiv);
 }
@@ -109,15 +126,33 @@ function printSetHead() {
   headSet.appendChild(setHead);
 }
 
+// function that will print five random sets and rarities per render
+const fiveRanSets = (arr) => {
+  const newInfo = []; // blank array to store our five items
+  const infoCopy = [...arr]; // copy of array that was passed in
+  // loop 5 times, remove one item from copy on each loop
+  for (let i = 0; i < 5; i++) {
+    // a random index with the arrays length
+    const randomIndex = Math.floor(Math.random() * infoCopy.length);
+    // splice item from copy and push into new array
+    newInfo.push(infoCopy.splice(randomIndex, 1)[0]);
+  }
+
+  // return array of 5 items after look
+  return newInfo;
+};
+
 // function to handle form submission for user
 function handleSubmit(event) {
-  document.querySelector('#price').innerHTML = '';
-  document.querySelector('#set').innerHTML = '';
+  document.querySelector("#price").innerHTML = "";
+  document.querySelector("#set").innerHTML = "";
+  
   event.preventDefault();
   const form = event.target;
   const formData = new FormData(form);
   const cardInfo = formData.get("cardName");
   getCard(cardInfo);
+  document.querySelector("#user-input").value = "";
 }
 
 //******************************** */
@@ -126,5 +161,6 @@ function handleSubmit(event) {
 // call for the targeted piece of data testing:
 // getCard("dark magician");
 // getCard("borreload savage dragon");
+
 // Event listener
 document.querySelector("form").addEventListener("submit", handleSubmit);
